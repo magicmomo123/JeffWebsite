f33b519e42d9e197966699380a0b086749cb971c
aryanallanthighall <bharath4@illinois.edu>
2022-04-16 19:46:32 -0500

    commit

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-// Select all the elements in the HTML page
-// and assign them to a variable
 let now_playing = document.querySelector(".now-playing");
 let track_art = document.querySelector(".track-art");
 let track_name = document.querySelector(".track-name");
@@ -14,118 +12,135 @@ let volume_slider = document.querySelector(".volume_slider");
 let curr_time = document.querySelector(".current-time");
 let total_duration = document.querySelector(".total-duration");
 
-// Specify globally used values
 let track_index = 0;
 let isPlaying = false;
 let updateTimer;
 
-// Create the audio element for the player
+// Create new audio element
 let curr_track = document.createElement('audio');
 
-// Define the list of tracks that have to be played
+// Define the tracks that have to be played
 let track_list = [
-{
-	name: "374",
-	artist: "Jayy b",
-	image: "Image URL",
-	path: "music/fuckmoney.wav"
-},
+  {
+    name: "Fuck Money",
+    artist: "theonlyjayyb",
+    image: "https://images.pexels.com/photos/2264753/pexels-photo-2264753.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
+    path: "music/fuckmoney.wav"
+  },
+  {
+    name: "Ocean Deep Thoughts",
+    artist: "theonlyjayyb",
+    image: "https://images.pexels.com/photos/3100835/pexels-photo-3100835.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
+    path: "OceanDeepThoughts.wav"
+  },
+  {
+    name: "The Only",
+    artist: "theonlyjayyb",
+    image: "https://images.pexels.com/photos/1717969/pexels-photo-1717969.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
+    path: "TheOnly.wav",
+  },
 ];
+
+function random_bg_color() {
+
+  // Get a number between 64 to 256 (for getting lighter colors)
+  let red = Math.floor(Math.random() * 256) + 64;
+  let green = Math.floor(Math.random() * 256) + 64;
+  let blue = Math.floor(Math.random() * 256) + 64;
+
+  // Construct a color withe the given values
+  let bgColor = "rgb(" + red + "," + green + "," + blue + ")";
+
+  // Set the background to that color
+  document.body.style.background = bgColor;
+}
+
 function loadTrack(track_index) {
-    // Clear the previous seek timer
-    clearInterval(updateTimer);
-    resetValues();
-    
-    // Load a new track
-    curr_track.src = track_list[track_index].path;
-    curr_track.load();
-    
-    // Update details of the track
-    track_art.style.backgroundImage =
-        "url(" + track_list[track_index].image + ")";
-    track_name.textContent = track_list[track_index].name;
-    track_artist.textContent = track_list[track_index].artist;
-    now_playing.textContent =
-        "PLAYING " + (track_index + 1) + " OF " + track_list.length;
-    
-    // Set an interval of 1000 milliseconds
-    // for updating the seek slider
-    updateTimer = setInterval(seekUpdate, 1000);
-    
-    // Move to the next track if the current finishes playing
-    // using the 'ended' event
-    curr_track.addEventListener("ended", nextTrack);
-    
-    // Apply a random background color
-    random_bg_color();
-    }
-    
-    function random_bg_color() {
-    // Get a random number between 64 to 256
-    // (for getting lighter colors)
-    let red = Math.floor(Math.random() * 256) + 64;
-    let green = Math.floor(Math.random() * 256) + 64;
-    let blue = Math.floor(Math.random() * 256) + 64;
-    
-    // Construct a color withe the given values
-    let bgColor = "rgb(" + red + ", " + green + ", " + blue + ")";
-    
-    // Set the background to the new color
-    document.body.style.background = bgColor;
-    }
-    
-    // Function to reset all values to their default
-    function resetValues() {
-    curr_time.textContent = "00:00";
-    total_duration.textContent = "00:00";
-    seek_slider.value = 0;
-    }
-    function playpauseTrack() {
-        // Switch between playing and pausing
-        // depending on the current state
-        if (!isPlaying) playTrack();
-        else pauseTrack();
-        }
-        
-        function playTrack() {
-        // Play the loaded track
-        curr_track.play();
-        isPlaying = true;
-        
-        // Replace icon with the pause icon
-        playpause_btn.innerHTML = '<i class="fa fa-pause-circle fa-5x"></i>';
-        }
-        
-        function pauseTrack() {
-        // Pause the loaded track
-        curr_track.pause();
-        isPlaying = false;
-        
-        // Replace icon with the play icon
-        playpause_btn.innerHTML = '<i class="fa fa-play-circle fa-5x"></i>';
-        }
-        
-        function nextTrack() {
-        // Go back to the first track if the
-        // current one is the last in the track list
-        if (track_index < track_list.length - 1)
-            track_index += 1;
-        else track_index = 0;
-        
-        // Load and play the new track
-        loadTrack(track_index);
-        playTrack();
-        }
-        
-        function prevTrack() {
-        // Go back to the last track if the
-        // current one is the first in the track list
-        if (track_index > 0)
-            track_index -= 1;
-        else track_index = track_list.length - 1;
-            
-        // Load and play the new track
-        loadTrack(track_index);
-        playTrack();
-        }
-          
+  clearInterval(updateTimer);
+  resetValues();
+  curr_track.src = track_list[track_index].path;
+  curr_track.load();
+
+  track_art.style.backgroundImage = "url(" + track_list[track_index].image + ")";
+  track_name.textContent = track_list[track_index].name;
+  track_artist.textContent = track_list[track_index].artist;
+  now_playing.textContent = "PLAYING " + (track_index + 1) + " OF " + track_list.length;
+
+  updateTimer = setInterval(seekUpdate, 1000);
+  curr_track.addEventListener("ended", nextTrack);
+  random_bg_color();
+}
+
+function resetValues() {
+  curr_time.textContent = "00:00";
+  total_duration.textContent = "00:00";
+  seek_slider.value = 0;
+}
+
+// Load the first track in the tracklist
+loadTrack(track_index);
+
+function playpauseTrack() {
+  if (!isPlaying) playTrack();
+  else pauseTrack();
+}
+
+function playTrack() {
+  curr_track.play();
+  isPlaying = true;
+  playpause_btn.innerHTML = '<i class="fa fa-pause-circle fa-5x"></i>';
+}
+
+function pauseTrack() {
+  curr_track.pause();
+  isPlaying = false;
+  playpause_btn.innerHTML = '<i class="fa fa-play-circle fa-5x"></i>';;
+}
+
+function nextTrack() {
+  if (track_index < track_list.length - 1)
+    track_index += 1;
+  else track_index = 0;
+  loadTrack(track_index);
+  playTrack();
+}
+
+function prevTrack() {
+  if (track_index > 0)
+    track_index -= 1;
+  else track_index = track_list.length;
+  loadTrack(track_index);
+  playTrack();
+}
+
+function seekTo() {
+  let seekto = curr_track.duration * (seek_slider.value / 100);
+  curr_track.currentTime = seekto;
+}
+
+function setVolume() {
+  curr_track.volume = volume_slider.value / 100;
+}
+
+function seekUpdate() {
+  let seekPosition = 0;
+
+  if (!isNaN(curr_track.duration)) {
+    seekPosition = curr_track.currentTime * (100 / curr_track.duration);
+
+    seek_slider.value = seekPosition;
+
+    let currentMinutes = Math.floor(curr_track.currentTime / 60);
+    let currentSeconds = Math.floor(curr_track.currentTime - currentMinutes * 60);
+    let durationMinutes = Math.floor(curr_track.duration / 60);
+    let durationSeconds = Math.floor(curr_track.duration - durationMinutes * 60);
+
+    if (currentSeconds < 10) { currentSeconds = "0" + currentSeconds; }
+    if (durationSeconds < 10) { durationSeconds = "0" + durationSeconds; }
+    if (currentMinutes < 10) { currentMinutes = "0" + currentMinutes; }
+    if (durationMinutes < 10) { durationMinutes = "0" + durationMinutes; }
+
+    curr_time.textContent = currentMinutes + ":" + currentSeconds;
+    total_duration.textContent = durationMinutes + ":" + durationSeconds;
+  }
+}
